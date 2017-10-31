94c499261341d15c6fa7267e41144a7e2bc232d1
MutualCoin <mutualcoin@protonmail.com>
2017-10-31 05:19:47 -0700

    changes

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define MUTUALCOIN-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "MUTUALCOINURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
